735c23e3ce96dfe95b30d04f29b7a3d01f31fdac
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2022-10-24 13:19:03 +0200

    First Version Zentangle

--- sketch_221024i/sketch_221024i.js
+++ sketch_221024i/sketch_221024i.js
@@ -1,8 +1,20 @@
+let x = 10;
+let y = 0;
+let spacing = 100;
 function setup() {
-
+createCanvas(windowWidth, windowHeight);
+background(0);
 }
-
-
 function draw() {
-
+  stroke(255);
+  if (random(1) < 0.5){ 
+    line(x, y, x + spacing, y + spacing);
+  } else {
+    line(x, y+spacing, x+spacing, y);
+  }
+  x = x + spacing;
+  if (x > width) {
+    x = 0;
+    y = y + spacing;
+  }
 }
