413bfa462e728f8fe64e2643d39cb49230b93ca8
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2022-10-24 13:54:07 +0200

    Update Zentangle

--- sketch_221024i/sketch_221024i.js
+++ sketch_221024i/sketch_221024i.js
@@ -1,18 +1,18 @@
-let x = 10;
+let x = 0;
 let y = 0;
-let spacing = 100;
+let abstand = 100;
 function setup() {
 createCanvas(windowWidth, windowHeight);
-background(0);
+background(255);
 }
 function draw() {
-  stroke(255);
+  stroke(0, 255, 255);
   if (random(1) < 0.5){ 
     line(x, y, x + spacing, y + spacing);
   } else {
-    line(x, y+spacing, x+spacing, y);
+    line(x, y+abstand, x+abstand, y);
   }
-  x = x + spacing;
+  x = x + abstand;
   if (x > width) {
     x = 0;
     y = y + spacing;
